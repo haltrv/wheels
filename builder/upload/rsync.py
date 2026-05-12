4a589c773dce2e0571124720879ad547e58d640b
E Shattow <e@freeshell.de>
2026-05-12 01:00:34 -0700

    NOT FOR UPSTREAM builder: upload: rsync remote exec index-503

--- builder/upload/rsync.py
+++ builder/upload/rsync.py
@@ -10,3 +10,12 @@ def upload(local: Path, remote: str) -> None:
     run_command(
         f"rsync --human-readable --recursive --progress --checksum {local}/* {remote}/",
     )
+    uhost, fpath = remote.split(":", 1)
+    run_command(
+        f"ssh {uhost} \'\n"
+        f"python3 -m venv index-503.venv\n"
+        f"source index-503.venv/bin/activate\n"
+        f"pip install index-503\n"
+        f"pushd {fpath} && index-503 musllinux && popd\n"
+        "\'"
+    )
